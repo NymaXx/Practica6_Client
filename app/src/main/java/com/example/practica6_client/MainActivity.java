b51f33654a3ac8f8a9e3f9d2da1475159a8e3cc9
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-03-17 14:24:37 -0500

    creacion de actividad e interfaz

--- app/src/main/java/com/example/practica6_client/MainActivity.java
+++ app/src/main/java/com/example/practica6_client/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.practica6_client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.BufferedWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button actionUP;
+    private Button actionDOWN;
+    private Button actionLEFT;
+    private Button actionRIGHT;
+    private Button actionCOLOR;
+
+
+    Socket socket;
+    BufferedWriter writer;
+    boolean isUP = true;
+    boolean isDOWN = true;
+    boolean isLEFT = true;
+    boolean isRIGHT = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionUP = findViewById(R.id.actionUP);
+        actionDOWN = findViewById(R.id.actionDOWN);
+        actionLEFT = findViewById(R.id.actionLEFT);
+        actionRIGHT = findViewById(R.id.actionRIGHT);
+        actionCOLOR = findViewById(R.id.actionCOLOR);
     }
 }
